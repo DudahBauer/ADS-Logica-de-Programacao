7294dffad5415510ba0bc4024846225d1d7d7ec6
Maria Eduarda <mevbauer@gmail.com>
2025-07-22 18:03:36 -0300

    Primeiro commit

---
+++ aulas_praticas/jogo_da_forca.py
@@ -0,0 +1,13 @@
+def borda(s1):
+    tam= len(s1)
+    if tam:
+        print("+", "-" * tam, "+")
+        print("|",s1,"|")
+        print("+", "-" * tam, "+")
+borda("JOGO-DA-FORCA")
+
+palavras=["gato", "casa", "sol", "livro", "bola", "amigo", "janela", "planeta", "escova", "camisa",
+    "abacaxi", "helicóptero", "xilofone", "maracujá", "quilombo", "pipoca", "escola", "telefone",
+    "carro", "girassol", "montanha", "praia", "computador", "cachorro", "elefante", "borboleta",
+    "chave", "futebol", "geladeira", "janela", "lápis", "mesa", "navio", "oceano", "palhaço"
+]
